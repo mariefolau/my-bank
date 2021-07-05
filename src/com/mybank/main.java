b9b9c5d6c2b1b3801c1ed92e5c6af1daa7b24f02
mariefolau <81399551+mariefolau@users.noreply.github.com>
2021-07-05 15:00:45 +1000

    Adding another method call to main

--- src/com/mybank/main.java
+++ src/com/mybank/main.java
@@ -4,7 +4,9 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MusicalChairs.playWithSorting();
+		
+		MusicalChairs.playWithMap();
 	}
 
 }
