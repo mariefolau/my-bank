ae254854683c2c1bb0ac39955eed3a0baa8cf41f
mariefolau <81399551+mariefolau@users.noreply.github.com>
2021-07-08 10:31:14 +1000

    Fixing files

--- src/com/mybank/main.java
+++ src/com/mybank/main.java
@@ -1,12 +1,31 @@
 package com.mybank;
 
-public class main {
+import com.mybank.account.CheckingAccount;
+import com.mybank.account.InsufficientFundsException;
+import com.mybank.account.SavingsAccount;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MusicalChairs.playWithSorting();
-		
-		MusicalChairs.playWithMap();
-	}
+public class Main {
 
+    public static void main(String[] args) {
+        CheckingAccount one = new CheckingAccount(
+                "customer one",
+                "customer one account",
+                100.0,
+                "c1"
+        );
+
+        SavingsAccount two = new SavingsAccount(
+                "saver two",
+                "saver two account",
+                100.0,
+                "s2"
+        );
+
+        try {
+            one.transfer(two, 5.0);
+            two.withdraw(3.0);
+        } catch (IllegalArgumentException | InsufficientFundsException | UnsupportedOperationException e) {
+            System.out.println(e);
+        }
+    }
 }
