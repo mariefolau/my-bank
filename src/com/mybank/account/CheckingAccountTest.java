e45cf47261388df62a55fc71183f84b1ad06498a
mariefolau <81399551+mariefolau@users.noreply.github.com>
2021-07-08 13:14:34 +1000

    Added first test

---
+++ src/com/mybank/account/CheckingAccountTest.java
@@ -0,0 +1,61 @@
+package com.mybank.account;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CheckingAccountTest {
+	// class under test
+	CheckingAccount account;
+
+	@BeforeEach
+	void setup() {
+		this.account = new CheckingAccount("customer 1", "test account", 0, "test account number");
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void deposit__amount_greater_than_zero__works() {
+
+		// try with good amount (greater than zero)
+		double amount = 42.0;
+
+		// do the actual test
+		account.deposit(amount);
+
+		// get the actual balance
+		double balance = account.getBalance();
+
+		// check it did work
+		assertEquals(amount, balance);
+
+	}
+
+	@Test
+	void deposit__amount_less_than_zero__throws() {
+		// bad amount
+		double amount = -1.0;
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			account.deposit(amount);
+		});
+
+	}
+
+	@Test
+	void deposit__amount_equal_to_zero__throws() {
+		// another bad amount
+		double amount = 0.0;
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			account.deposit(amount);
+		});
+
+	}
+}
